f212875959042944ccc2ccdc87a189b506446495
Bosssmann810 <sealered@gmail.com>
2026-09-14 16:15:33 -0300

    end gate script bugs fixed and startgate and endgate moved to be on the platform

--- Assets/Scripts/EndGate.cs
+++ Assets/Scripts/EndGate.cs
@@ -2,17 +2,13 @@ using UnityEngine;
 
 public class EndGate : MonoBehaviour
 {
-    public class StartGate : MonoBehaviour
+    [SerializeField] TimerScript m_timer;
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        [SerializeField] TimerScript m_timer;
-
-
-        public void OnTriggerEnter2D(Collider2D collision)
+        if (collision.CompareTag("Player"))
         {
-            if (collision.CompareTag("Player"))
-            {
-                m_timer.StopTimer();
-            }
+            m_timer.StopTimer();
         }
     }
 }
+
