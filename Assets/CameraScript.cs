a279f26d7fe7d4a14fcd066f5d5995e2da691b18
Gage Mckenzie <gagemckenzie70@gmail.com>
2026-09-22 09:57:28 -0300

    camera follows player

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    public Transform cameraMin;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y <= cameraMin.transform.position.y)
+        {
+            gameObject.transform.position = new Vector3(player.transform.position.x, cameraMin.transform.position.y + 2f, gameObject.transform.position.z);
+        }
+        else
+        {
+            gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, gameObject.transform.position.z);
+        }
+        
+         
+        
+    }
+}
