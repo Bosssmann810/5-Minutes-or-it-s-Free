07ab8654b19209e6a99e0e5a7d4aae87cd522418
Gage Mckenzie <gagemckenzie70@gmail.com>
2026-09-25 10:47:59 -0300

    fixed ending
    you can't move if you make it to the end

--- Assets/PauseManager.cs
+++ Assets/PauseManager.cs
@@ -105,4 +105,12 @@ public class PauseManager : MonoBehaviour
             
         }
     }
+
+    public void DisableGame()
+    {
+        meter.enabled = false;
+        player.enabled = false;
+        input.enabled = false;
+        spriteAnimator.enabled = false;
+    }
 }
--- Assets/Scripts/EndGate.cs
+++ Assets/Scripts/EndGate.cs
@@ -4,12 +4,14 @@ public class EndGate : MonoBehaviour
 {
     [SerializeField] TimerScript m_timer;
     public GameManager m_gameManager;
+    public PauseManager pause;
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             m_timer.StopTimer();
             m_gameManager.GameWon();
+            pause.DisableGame();
         }
     }
 }
